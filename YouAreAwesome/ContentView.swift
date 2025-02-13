844b4c027541d12a2e6b986f878670f57a91de41
Chris Wheeler <cwheeler@emeals.com>
2025-02-13 16:39:18 -0500

    Chapter 1.9 Array challenge

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var message = "You Are Awesome!"
+    @State private var message = ""
     @State private var imageNumber = 1
+    @State private var messageNumber = 0
 
     let imagePrefixString = "moveInWeekend - "
     
@@ -31,14 +32,23 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
             
             Spacer()
             
             Button("Press Me!") {
-                let message0 = "You are Great!"
-                let message1 = "You are Awesome!"
+                let messages = ["You are Awesome!",
+                                "You are Great!",
+                                "You are Magical!",
+                                "You are a Really Long Text String!",
+                                "Stop Chanting!"]
                 
-                message = ( message == message0 ? message1 : message0)
+                message = messages[messageNumber]
+                messageNumber += 1
+
+                if messageNumber >= messages.count {
+                    messageNumber = 0
+                }
 
                 imageNumber += 1
                 if imageNumber > 10 {
